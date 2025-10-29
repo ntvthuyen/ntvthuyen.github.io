e266422a088cfd7f4aae7b6bf224040d98d8302f
Vinh-Thuyen <34017414+ntvthuyen@users.noreply.github.com>
2025-10-29 11:13:27 +0800

    Update icon.ts

--- src/lib/config/icon.ts
+++ src/lib/config/icon.ts
@@ -11,17 +11,17 @@ export const favicon: Icon = {
 export const any: { [key: number]: Icon } = {
   180: {
     sizes: '180x180',
-    src: `${site.protocol + site.domain}/assets/any@180.png`,
+    src: `${site.protocol + site.domain}/assets/penguin@180.png`,
     type: 'image/png',
   },
   192: {
     sizes: '192x192',
-    src: `${site.protocol + site.domain}/assets/any@192.png`,
+    src: `${site.protocol + site.domain}/assets/penguin@192.png`,
     type: 'image/png',
   },
   512: {
     sizes: '512x512',
-    src: `${site.protocol + site.domain}/assets/any@512.png`,
+    src: `${site.protocol + site.domain}/assets/penguin@512.png`,
     type: 'image/png',
   },
 }
@@ -29,12 +29,12 @@ export const any: { [key: number]: Icon } = {
 export const maskable: { [key: number]: Icon } = {
   192: {
     sizes: '192x192',
-    src: `${site.protocol + site.domain}/assets/maskable@192.png`,
+    src: `${site.protocol + site.domain}/assets/penguin@192.png`,
     type: 'image/png',
   },
   512: {
     sizes: '512x512',
-    src: `${site.protocol + site.domain}/assets/maskable@512.png`,
+    src: `${site.protocol + site.domain}/assets/penguin@512.png`,
     type: 'image/png',
   },
 }
