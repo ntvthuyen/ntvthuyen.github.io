0d54646624d98ba9dd4d30b256bdce759baa9c31
Vinh-Thuyen <34017414+ntvthuyen@users.noreply.github.com>
2026-06-29 18:11:18 +0800

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -5,13 +5,13 @@ export const site: SiteConfig = {
     avatar: '/assets/penguin.jpg',
     name: 'Vinh-Thuyen',
     status: '🐧',
-    bio: 'A penguin \n email: thuyen@nus.edu.sg, ntvthuyen@{gmail(dot)com, apcs.fitus(dot)edu.vn}'
+    bio: 'Email: vthuyen@u(dot)nus(dot)edu, ntvthuyen@{gmail(dot)com, apcs.fitus(dot)edu.vn}'
   },
   description: 'Powered by SvelteKit/Urara',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'ntvthuyen.github.io',
   lang: 'en-US',
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  subtitle: 'A penguin loves coding, music, and science',
+  subtitle: 'I love penguins, cats, coding, music, and science',
   themeColor: '#3D4451',
   title: 'Vinh-Thuyen',
 }
